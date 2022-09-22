c5eeaf96b47db46a3406d3aef4dc19c8ee903d3e
vscode_tfa <guilhermesr2000@gmail.com>
2022-09-22 11:53:41 -0300

    feat: makefile

---
+++ main.d
@@ -0,0 +1,4 @@
+obj/main.o: /home/guirab/Documents/processamento-graf/src/main.cpp \
+ /home/guirab/Documents/processamento-graf/src/include/glad/glad.h \
+ /home/guirab/Documents/processamento-graf/src/include/glad/../KHR/khrplatform.h \
+ /home/guirab/Documents/processamento-graf/src/include/GLFW/glfw3.h
